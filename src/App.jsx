87f11549414ffcb738d6e93624a50a47f35717c2
HoangKhoi1005 <mrkhoibl789@gmail.com.com>
2024-02-05 03:34:29 +0700

    Config Mui Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import Button from '@mui/material/Button';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
-import HomeIcon from '@mui/icons-material/Home';
-import { pink } from '@mui/material/colors';
+import Button from '@mui/material/Button'
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
     <>
       <div>hoangkhoidev</div>
+
+      <Typography variant="body2" color="text.secondary">
+        Text Typography
+      </Typography>
       <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>    
-      
+      <Button variant="contained" color="success">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
       <br></br>
       <AccessAlarmIcon></AccessAlarmIcon>
       <ThreeDRotation></ThreeDRotation>
