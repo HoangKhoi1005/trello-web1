69b4ca9ffc026cd44be5cd78c060aeb7d3ba3c0f
HoangKhoi1005 <mrkhoibl789@gmail.com>
2024-07-05 00:29:43 +0700

    Relative Absolute Import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import Board from './pages/Boards/_id'
-
+import Board from '~/pages/Boards/_id'
 
 
 function App() {
   return (
     <>
+      {/* React Router Dom /boards /boards/{board_id} */}
+      {/* Board Details */}
       <Board />
     </>
   )
