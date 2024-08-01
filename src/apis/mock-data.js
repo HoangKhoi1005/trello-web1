9fd07f8f26129a3b610c66829596760a143852a6
HoangKhoi1005 <mrkhoibl789@gmail.com>
2024-08-01 17:44:09 +0700

    Fix Bug Empty Column

--- src/apis/mock-data.js
+++ src/apis/mock-data.js
@@ -7,7 +7,7 @@ export const mockData = {
     type: 'public', // 'private'
     ownerIds: [], // Những users là Admin của board
     memberIds: [], // Những users là member bình thường của board
-    columnOrderIds: ['column-id-01', 'column-id-02', 'column-id-03'], // Thứ tự sắp xếp / vị trí của các Columns trong 1 boards
+    columnOrderIds: ['column-id-01', 'column-id-02', 'column-id-03', 'column-id-04'], // Thứ tự sắp xếp / vị trí của các Columns trong 1 boards
     columns: [
       {
         _id: 'column-id-01',
@@ -54,6 +54,28 @@ export const mockData = {
           { _id: 'card-id-11', boardId: 'board-id-01', columnId: 'column-id-03', title: 'Title of card 11', description: null, cover: null, memberIds: [], comments: [], attachments: [] },
           { _id: 'card-id-12', boardId: 'board-id-01', columnId: 'column-id-03', title: 'Title of card 12', description: null, cover: null, memberIds: [], comments: [], attachments: [] },
           { _id: 'card-id-13', boardId: 'board-id-01', columnId: 'column-id-03', title: 'Title of card 13', description: null, cover: null, memberIds: [], comments: [], attachments: [] }
+        ]
+      },
+      {
+        _id: 'column-id-04',
+        boardId: 'board-id-01',
+        title: 'Empty Column 04',
+        /**
+         * Video 37.2: Xử lý bug logic thư viện dnd-kit khi column rỗng:
+         * Phía FE sẽ tạo 1 card đặc biệt: Placeholder Card, không liên quan đến BE
+         * Card này sẽ ẩn trên UI người dùng
+         * Cấu trúc Id của card này để Unique rất đơn giản, không làm random như các card thông thường
+         * "columnId-placeholder-card": mỗi column có tối đa 1 Placeholder Card
+         * Quan trọng là khi tạo phải đầy đủ: (id, boardId, columnId, FE_PlaceholderCard: true)
+         */
+        cardOrderIds: ['column-id-04-placeholder-card'],
+        cards: [
+          {
+            _id: 'card-id-111',
+            boardId: 'board-id-01',
+            columnId: 'column-id-04',
+            FE_PlaceholderCard: true
+          }
         ]
       }
     ]
